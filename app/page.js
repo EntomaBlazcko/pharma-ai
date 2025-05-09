2db3cc0fc2be78cbe9c8fc3ec63a226dd5e8fb6b
EntomaBlazcko <maducdoc4@gmail.com>
2025-05-09 10:51:55 +0800

    ANOTHER TEST

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
         <div className="w-full lg:w-3/4 p-8">
           <h2 className="text-2xl font-bold text-[#18442A]">ABOUT US</h2>
           <p className="mt-4 text-gray-600">
-            THIS WILL BE THE CONTACT PLACEHOLDER.
+            THIS WILL BE THE CONTACT test test test test
           </p>
         </div>
       </div>
